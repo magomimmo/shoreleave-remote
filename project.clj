173d517ff2381a6b33af60c058c8002c0898d553
magomimmo <mimmo.cosenza@gmail.com>
2013-11-23 09:01:40 +0100

    prepare for clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.magomimmo/shoreleave-remote "0.3.1-SNAPSHOT"
+(defproject org.clojars.magomimmo/shoreleave-remote "0.3.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's rpc/xhr/jsonp facilities"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
-  :dependencies [[org.clojars.magomimmo/shoreleave-core "0.3.1-SNAPSHOT"]
-                 [org.clojars.magomimmo/shoreleave-browser "0.3.1-SNAPSHOT"]])
+  :dependencies [[org.clojars.magomimmo/shoreleave-core "0.3.1"]
+                 [org.clojars.magomimmo/shoreleave-browser "0.3.1"]])
 
